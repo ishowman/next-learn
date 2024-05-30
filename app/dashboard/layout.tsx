a69fb4bbbf6004c364642d0b401c1e32d9525a71
killer <killer@fbi.com>
2024-05-30 10:11:55 +0000

    feat: chapter5

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,6 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         {/* chapter4 测试点击 nav 的页面跳转，刷新按钮会转动（spa 页面跳转刷新按钮不会动），原因可能是 nav-links.tsx 中使用了 a 标签*/}
+        {/* chapter5 会解决这个问题 */}
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
